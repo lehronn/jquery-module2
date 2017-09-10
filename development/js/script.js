449c91be2c8b0a01b29da6f97e0ffdf9ba449a0f
Mateusz Stomski <lehronn@gmail.com>
2017-09-10 13:32:48 +0200

    html and css is ready.

--- development/js/script.js
+++ development/js/script.js
@@ -5,22 +5,4 @@ $(function() {
   console.log('js file is started and DOMs objects is loaded.');
   //end waiting
 
-  //change color with css metod
-  var spans = $('span');
-  spans.each(function(index, element) {
-    if (index % 2 == 0) {
-      $(element).css('color', 'red');
-    }
-  });
-
-  var paragraphs = $('p');
-  paragraphs.each(function(index, element) {
-    var btn = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-    $(element).append(btn);
-  });
-
-  $('button').click(function(){
-    alert($(this).attr("data-tmp"));
-  });
-
 });
