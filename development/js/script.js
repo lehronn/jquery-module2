68cb30a25ef91174303c42bd24343c262d68ccb1
Mateusz Stomski <lehronn@gmail.com>
2017-09-11 18:33:19 +0200

    secound exercise is ready

--- development/js/script.js
+++ development/js/script.js
@@ -5,4 +5,18 @@ $(function() {
   console.log('js file is started and DOMs objects is loaded.');
   //end waiting
 
+  var carouselList = $("#carousel ul");
+
+  var changeSlide = function() {
+  setInterval(changeSlide, 3000);
+  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+};
+
+  var moveFirstSlide = function() {
+    var firstItem = carouselList.find("li:first"),
+        lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  changeSlide();
 });
